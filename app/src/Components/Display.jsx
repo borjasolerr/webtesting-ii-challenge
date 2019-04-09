19a3f6736f40b73a3f8d2a20fce7e42e6b9dca4b
borjasolerr <borjasolrey@gmail.com>
2019-04-09 17:05:18 +0200

    Added dashboard component

---
+++ app/src/Components/Display.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Display extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h2>Display</h2>
+      </div>
+    );
+  }
+}
+
+export default Display;
